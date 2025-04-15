7269753048f40124a185ca0fc23fcb9ee98b067a
kakahimita <kakahimita@gmail.com>
2025-04-15 18:07:01 -0300

    feat: added index.js for test coding

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 import { FaixaEtaria } from "./src/models/FaixaEtaria.js";
+import { Genero } from "./src/models/Genero.js";
+import { Pessoa } from "./src/models/Pessoa.js";
+import { DVD } from "./src/models/DVD.js";
+import { Amigo } from "./src/models/Amigo.js";
 
-const faixaAmigo = new FaixaEtaria(8, 15);
-const faixaDVD = new FaixaEtaria(18, 99)
+// Import readline-sync
+import readlineSync from 'readline-sync';
 
-console.log(faixaAmigo.ehAbaixo(faixaDVD));
+// --- Inicializar o sistema ---
+const amigos = [];
+const dvds = [];
+
+// --- Loop Principal (Menu Interativo) ---
+let rodando = true;
+
+while (rodando) {
+    console.log("\n--- Menu Principal ---");
+    console.log("1. Cadastrar Amigo");
+    console.log("2. Cadastrar DVD");
+    console.log("0. Sair");
+
+    const opcao = readlineSync.question('Escolha uma opção: ');
+
+    switch (opcao) {
+        case '1':
+            // Cadastrar Amigo
+            console.log("\n--- Cadastrar Novo Amigo ---");
+            const nomeAmigo = readlineSync.question('Nome do amigo: ');
+            const numTelefone = readlineSync.question('Telefone: ');
+            const email = readlineSync.question('E-mail: ');
+            const endereco = readlineSync.question('Endereco: ');
+
+            const novoAmigo = new Amigo(nomeAmigo, numTelefone, email, endereco);
+
+            amigos.push(novoAmigo);
+
+            console.log("cadastrado com sucesso!")
+            console.log(novoAmigo);
+            break;
+    }
+}
