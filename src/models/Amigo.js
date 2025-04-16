4024eeabe5bf65df6d93526ee3c5ed5deface2ef
kakahimita <kakahimita@gmail.com>
2025-04-16 20:41:34 -0300

    fix: Amigo class correction

--- src/models/Amigo.js
+++ src/models/Amigo.js
@@ -1,9 +1,10 @@
 import { Pessoa } from "./Pessoa.js";
 export class Amigo extends Pessoa {
-    constructor(nome, numTelefone, email, endereco) {
+    constructor(nome, numTelefone, email, endereco, faixaEtaria) {
         super(nome);
         this.numTelefone = numTelefone;
         this.email = email;
         this.endereco = endereco;
+        this.faixaEtaria = faixaEtaria;
     }
 }
